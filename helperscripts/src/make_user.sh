745bb4c0de831ade2d4f52762aaeadb0e104e559
Fabian Brand <brand@imbie.uni-bonn.de>
2020-07-16 11:46:17 +0200

    Creating switches to disable db and initial user creation.

--- helperscripts/src/make_user.sh
+++ helperscripts/src/make_user.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-echo "Creating initial user..."
+if [[ $INIT_USER = "1" ]]; then
+  echo "Creating initial user..."
 
-echo -e "Username:\t$INITIAL_USER"
-HASH=$(perl -w /src/hash_pw.pl "$INITIAL_USER_PASSWD")
+  echo -e "Username:\t$INITIAL_USER"
+  HASH=$(perl -w /src/hash_pw.pl "$INITIAL_USER_PASSWD")
 
-QUERY_FILE=$(mktemp)
-echo -e "insert into exomevcfe.user (name,password,role,edit,genesearch,yubikey) VALUES ('$INITIAL_USER', '$HASH', 'admin', 1, 1, 0);" > $QUERY_FILE
+  QUERY_FILE=$(mktemp)
+  echo -e "insert into exomevcfe.user (name,password,role,edit,genesearch,yubikey) VALUES ('$INITIAL_USER', '$HASH', 'admin', 1, 1, 0);" > $QUERY_FILE
 
-mysql -L -u ${DB_USER} -p${DB_PASSWD} -h ${DB_HOST} $DB < $QUERY_FILE
+  mysql -L -u ${DB_USER} -p${DB_PASSWD} -h ${DB_HOST} $DB < $QUERY_FILE
+fi
